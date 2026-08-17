14e2be17a03e091674fc2b5f6d33851b8ef62fb7
ZQG <2721499274@qq.com>
2026-08-17 15:28:07 +0800

    修改

--- drsr_420/find_best_eq.py
+++ drsr_420/find_best_eq.py
@@ -84,7 +84,7 @@ def expr_substitution(func: str, params: list) ->  sp.Expr | None:
     pattern = "\"\"\"(.*?)\"\"\"|#[^\n]*"  # 非贪婪匹配
     func = re.sub(pattern, "", func, flags=re.DOTALL)  # 将匹配到的内容替换为""
 
-    # 将np.替换为sp. 将maximum 替换为 Piecewise
+    # 将np.替换为sp. 将maximum替换为Max 将where替换为Piecewise
     func = func.replace("np.","").replace("maximum", "Max").replace("minimum", "Min")
 
     inter_vars = {}
